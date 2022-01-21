5dbd33e5bdd9b950874f6574c0d3c1daeac58ff6
Swati Adhikari <45826182+sadhika7@users.noreply.github.com>
2022-01-20 20:17:55 -0500

    Add files via upload

---
+++ instagram/defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '670326907739859' );
+	define( 'INSTAGRAM_APP_SECRET', '613d06816447525b269c4c66a4e66abc');
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'http://localhost:8080/Redirect/' );
